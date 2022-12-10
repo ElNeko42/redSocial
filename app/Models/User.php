6d40bb0df5b7b6f5d3b789cc66cff992f3d56f4f
ElNeko42 <franson177@gmail.com>
2022-12-10 21:20:03 +0100

    modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,11 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function post(){
+        return $this->hasmany(Posts::class);
+    }
+    public function followers(){
+        return $this->hasmany(Followers::class);
+    }
 }
