59a4d91e3f792db9df0e16a76f7ad93cf656ca27
ElNeko42 <franson177@gmail.com>
2022-12-29 14:15:45 +0100

    controllador post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Likes;
+use App\Models\Posts;
+use App\Models\Comments;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
-    //
+    private $post;
+    private $comments;
+    private $like;
+
+    public function __construct(Posts $post,Comments $comments,Likes $likes)
+    {
+        $this->post=$post;
+        $this->comments=$comments;
+        $this->like=$likes;
+    }
+    public function createPost(Request $request){
+        try{
+            DB::beginTransaction();
+            $post=$this->post->createPost($request);
+            DB::commit();
+            return$post;
+        }
+        catch(\Exception $e){
+            DB::rollBack();
+            return response()->json($e->getMessage(),500);
+        }
+    }
 }
